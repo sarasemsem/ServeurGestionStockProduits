02722b471dc2e9ff6f9986d4855bd5b069b09445
Sara Mejbri <mejbrisara@gmail.com>
2023-11-09 00:28:24 +0100

    initialisation du projet

---
+++ src/main/java/com/ServeurGestionStockProduits/ServeurGestionStockProduits/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.ServeurGestionStockProduits.ServeurGestionStockProduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ServeurGestionStockProduits/ServeurGestionStockProduits/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServeurGestionStockProduits.ServeurGestionStockProduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
